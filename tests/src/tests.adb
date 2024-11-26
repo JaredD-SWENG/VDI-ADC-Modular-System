57da1439ce6016875785f11f1bcf1634dfc17add
Haris5097 <hhs5097@psu.edu>
2024-11-25 21:36:27 -0500

    Completed Image Comparison Tester

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -1,68 +1,79 @@
-with QOI;
-with System.Storage_Elements; use System.Storage_Elements;
-with Load_QOI;                use Load_QOI;
--- with Ada.Strings;             use Ada.Strings;
--- with Ada.Text_IO;             use Ada.Text_IO;
+with Ada.Command_Line;
+with GNAT.OS_Lib;
+with Ada.Text_IO; use Ada.Text_IO;
 
-with Opencv_Ada.Colorspace;      use Opencv_Ada.Colorspace;
-with Opencv_Ada.Edge_Detection;  use Opencv_Ada.Edge_Detection;
-with Opencv_Ada.Hough_Transform; use Opencv_Ada.Hough_Transform;
+with QOI;
+with AAA.Strings;
+with System.Storage_Elements; use System.Storage_Elements;
+
+with Load_QOI; use Load_QOI;
 
 procedure Tests is
-   -- Input_File_Name   : String := "src\images\" & "5x5BlackSquare.qoi";
-   -- Input_File_Name  : String := "src\images\" & "hi144p.qoi";
-   Input_File_Name  : constant String := "src\images\" & "lane1.qoi";
-   Output_File_Name : constant String := "output.qoi";
-   Input            : Input_Data;
+   Input1 : Input_Data;
+   Input2 : Input_Data;
 begin
-   Input := Get_QOI (Input_File_Name);
+   if Ada.Command_Line.Argument_Count /= 2 then
+      Put_Line (Standard_Error, "Usage: test_input_image_buffer <testing image> <actual image>");
+      GNAT.OS_Lib.OS_Exit (1);
+   end if;
 
-   ----------------- PRE-REQs ---------------- (Function Pre-requisites)
-   -- Convert to grayscale first
-   Convert_To_Grayscale (Input.Data.all, Input.Desc);
+   if AAA.Strings.Has_Suffix (Ada.Command_Line.Argument (1), ".qoi")
+     and AAA.Strings.Has_Suffix (Ada.Command_Line.Argument (2), ".qoi")
+   then
+      Put_Line ("QOI Image 1: " & Ada.Command_Line.Argument (1));
+      Put_Line ("QOI Image 2: " & Ada.Command_Line.Argument (2));
+      Input1 := Get_QOI (Ada.Command_Line.Argument (1));
+      Input2 := Get_QOI (Ada.Command_Line.Argument (2));
+   else
+      Put_Line (Standard_Error, "File extension can only be '.qoi' for both!");
+      GNAT.OS_Lib.OS_Exit (1);
+   end if;
 
-   -- Blur the image
-   --  Blur_Image
-   --    (Input.Data.all,
-   --     Input.Desc.Width,
-   --     Input.Desc.Height,
-   --     Input.Desc.Channels);
-
-   -- Apply Sobel edge detection
-   Sobel_Edge_Detection
-     (Input.Data.all,
-      Input.Desc.Width,
-      Input.Desc.Height,
-      Input.Desc.Channels);
-
-   -- Apply Canny edge detection
-   Canny_Edge_Detection
-     (Input.Data.all,
-      Input.Desc.Width,
-      Input.Desc.Height,
-      Input.Desc.Channels,
-      Low_Threshold  => 0.1,
-      High_Threshold => 0.3);
-
-   -- Apply Hough Transform to detect lines
-   Hough_Line_Transform
-     (Input.Data.all,
-      Input.Desc.Width,
-      Input.Desc.Height,
-      Input.Desc.Channels);
-   ------------------  END  ------------------
+   if Input1.Desc.Width * Input1.Desc.Height /= Input2.Desc.Width * Input2.Desc.Height
+   then
+      Put_Line ("Image sizes are different!");
+      GNAT.OS_Lib.OS_Exit (1);
+   end if;
 
    declare
-      Output      : Storage_Array (1 .. QOI.Encode_Worst_Case (Input.Desc));
-      -- Output_Temp : Storage_Array (1 .. Input.Desc.Width * Input.Desc.Height * Input.Desc.Channels) := (others => 150);
-      Output_Size : Storage_Count;
-
-      ----------------- START ----------------- (Function Testing)
-
-      -----------------  END  -----------------
+      Output           : Storage_Array (1 .. QOI.Encode_Worst_Case (Input1.Desc));
+      Output_Size      : Storage_Count;
+      Difference_Found : Integer := 0;
+      Index            : Storage_Offset := 1;
    begin
+      while Index <= Input1.Desc.Width * Input1.Desc.Height * Input1.Desc.Channels loop
+         if Input1.Data (Index) = Input2.Data (Index)
+           and Input1.Data (Index + 1) = Input2.Data (Index + 1)
+           and Input1.Data (Index + 2) = Input2.Data (Index + 2)
+         then
+            Input1.Data (Index) := 0;
+            Input1.Data (Index + 1) := 0;
+            Input1.Data (Index + 2) := 0;
+         else
+            Input1.Data (Index) := 255;
+            Input1.Data (Index + 1) := 0;
+            Input1.Data (Index + 2) := 0;
+            Difference_Found := 1;
+            Put_Line
+              ("Difference: " 
+               & "[" & Input1.Data (Index)'Image & ", " & Input1.Data (Index + 1)'Image & ", " & Input1.Data (Index + 2)'Image & "]" 
+               & " != "
+               & "[" & Input2.Data (Index)'Image & ", " & Input2.Data (Index + 1)'Image & ", " & Input2.Data (Index + 2)'Image & "]");
+         end if;
+         Index := Index + 3;
+      end loop;
 
-      QOI.Encode (Input.Data.all, Input.Desc, Output, Output_Size);
-      Write_To_File (Output_File_Name, Output, Output_Size);
+      if Difference_Found = 0 then
+         Put_Line ("Image are the same.");
+      end if;
+
+      QOI.Encode (Input1.Data.all, Input1.Desc, Output, Output_Size);
+
+      if Output_Size /= 0 then
+         Write_To_File ("src\test_cases\" & "image_comparison.qoi", Output, Output_Size);
+      else
+         Put_Line ("Encode failed");
+         GNAT.OS_Lib.OS_Exit (1);
+      end if;
    end;
 end Tests;
