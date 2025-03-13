d3a808ae291b43ded9945f52542e02f3a312f451
Ryan Truax <Truax53@live.com>
2025-03-13 12:03:39 -0400

    Add steering priority and period to system config

--- hw_refactor/src/system_config.ads
+++ hw_refactor/src/system_config.ads
@@ -1,7 +1,9 @@
 with System; use System;
 package System_Config is
    Motor_Priority : constant Priority := Priority'First;
-   Uart_Priority  : constant Priority := Motor_Priority + 1;
+   Steering_Priority : constant Priority := Motor_Priority + 1;
+   Uart_Priority  : constant Priority := Steering_Priority + 1;
    Motor_Period : constant := 1000;
+   Steering_Period : constant := 1000;
    Uart_Period  : constant := 100;
 end System_Config;
