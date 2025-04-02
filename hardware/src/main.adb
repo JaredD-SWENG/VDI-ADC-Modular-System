012bffe8a467b8e30013c28d12207ebdfa2fa91f
“Kevin <kpf5297@psu.edu>
2025-04-02 16:46:02 -0400

    Refactor motor test implementation

--- hardware/src/main.adb
+++ hardware/src/main.adb
@@ -1,6 +1,6 @@
 with Uart; use Uart;
 with Motor;
-with Steering;
+--with Steering;
 with STM32.Board;
 
 with Ada.Real_Time; use Ada.Real_Time;
@@ -9,38 +9,27 @@ procedure Main is
    Period  : constant Time_Span := Milliseconds (10);
    Next_Release  : Time := Clock;
    C : Uart.Cmd;
-   GoSpeed : Integer := 25;
-   StopSpeed : Integer := 0;
 begin
    STM32.Board.Initialize_LEDs;
    Motor.Init;
-   Steering.Init;
    loop
       C := Uart.Get_Command;
       case (C) is
-         when center => 
-            Steering.Set_Angle_Steering1(7);
-            null;
-         when left =>
-            Steering.Set_Angle_Steering1(8);
-            null;
-         when right => 
-            Steering.Set_Angle_Steering1(6);
+         when center | left | right => 
+            --  Steering.Set_Command(Cmd);
             null;
          when go =>
             STM32.Board.Turn_Off (STM32.Board.Red_LED);
             STM32.Board.Turn_On (STM32.Board.Green_LED);
             Motor.Set_Speed_Motor_1 (25);
-
          when stop =>
             STM32.Board.Turn_On (STM32.Board.Red_LED);
             STM32.Board.Turn_Off (STM32.Board.Green_LED);
             Motor.Set_Speed_Motor_1 (0);
-     
          when undefined => 
             null;
       end case;
-      
+
       Next_Release := Next_Release + Period;
       delay until Next_Release;
    end loop;
--- hardware/src/system_config.ads
+++ hardware/src/system_config.ads
@@ -6,12 +6,12 @@ with STM32.PWM; use STM32.PWM;
 with STM32.Device; use STM32.Device;
 
 package System_Config is
-   Motor_Priority : constant Priority := Priority'First + 2;
-   Steering_Priority : constant Priority := Priority'First + 1;
+   Steering_Priority : constant Priority := Priority'First + 2;
+   Motor_Priority : constant Priority := Priority'First + 1;
    Uart_Priority  : constant Priority := Priority'First;
 
-   Motor_Period : constant := 100;
-   Steering_Period : constant := 100;
+   Motor_Period : constant := 400;
+   Steering_Period : constant := 400;
    Uart_Period  : constant := 200;
 
 end System_Config;
